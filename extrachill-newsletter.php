35a36f6a7c645b8e506454df40c194bda23f6e21
Chris Huber <chubes4@users.noreply.github.com>
2026-01-23 21:23:51 -0600

    release: v0.2.3

--- extrachill-newsletter.php
+++ extrachill-newsletter.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Extra Chill Newsletter
  * Description: Complete newsletter system with Sendy integration for email campaigns and subscriptions. Provides custom newsletter post type, multiple subscription forms, email template generation, and admin management tools.
- * Version: 0.2.2
+ * Version: 0.2.3
  * Author: Chris Huber
  * Network: true
  * Text Domain: extrachill-newsletter
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'EXTRACHILL_NEWSLETTER_VERSION', '0.2.2' );
+define( 'EXTRACHILL_NEWSLETTER_VERSION', '0.2.3' );
 define( 'EXTRACHILL_NEWSLETTER_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'EXTRACHILL_NEWSLETTER_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'EXTRACHILL_NEWSLETTER_INC_DIR', EXTRACHILL_NEWSLETTER_PLUGIN_DIR . 'inc/' );
